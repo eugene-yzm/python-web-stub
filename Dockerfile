86487936af00bfe6433e9e62e5384c75452c722b
Eugene Ma <eugene@luminary.audio>
2019-10-21 17:04:46 -0400

    Add docker-compose and light main app

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM python:alpine3.10
 
 ENV PYTHONUNBUFFERED 1
 
+EXPOSE 5000
+
 WORKDIR /app
 
-COPY src/requirements.txt ./
+COPY * /app/
 
 RUN pip3 install -r requirements.txt
 
-CMD [ "python3" ]
+CMD [ "python3",  "main.py" ]
---
+++ src/main.py
@@ -0,0 +1,21 @@
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+def setup_routes():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+def main():
+    app = setup_routes()
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(5000)
+    tornado.ioloop.IOLoop.current().start()
+
+if __name__ == "__main__":
+    main()
